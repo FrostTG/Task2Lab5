ead0f61ee41d79360a283fa62159dc98cc51b156
Anton Gusev <FrostTG@gmail.com>
2021-10-12 13:51:51 +0300

    Добавьте файлы проекта.

---
+++ Task2Lab5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 15;
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 50);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int max = array[0]; //почему присваиваем значение по массиву? если написать max=0 будет тоже самое ведь?
+            int min = array[0];
+            foreach (int a in array)
+            {
+                if (a>max)
+                {
+                    max = a;
+                }
+                if (a < min)
+                {
+                    min = a;
+                }                
+            }
+            Console.WriteLine("Максимальное число = {0}",max);
+            Console.WriteLine("Минимальное число = {0}", min);
+            Console.WriteLine("Сумма max и min чисел = {0}",max+min);
+            Console.ReadKey();
+        }
+    }
+}
